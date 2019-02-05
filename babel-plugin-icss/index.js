87ec07005cf048dc8958ca194c3a10b305cd0021
Danil Semelenov <mail@danil.mobi>
2019-02-06 02:46:31 +0300

    Remove support for a regex-like tag option for simplification

--- babel-plugin-icss/index.js
+++ babel-plugin-icss/index.js
@@ -2,22 +2,12 @@
 const { css } = require('icss');
 
 module.exports = ({ types }, options = {}) => {
-  const tag =
-    options.tag && options.tag[0] === '/'
-      ? new RegExp(
-          options.tag.replace(/^\/(.*)\/.*?$/, '$1'),
-          options.tag.replace(/^\/.*\//, '')
-        )
-      : options.tag || 'css';
+  const tag = options.tag || 'css';
   return {
     name: 'htm',
     visitor: {
       TaggedTemplateExpression: path => {
-        if (
-          typeof tag === 'string'
-            ? path.node.tag.name === tag
-            : tag.test(path.node.tag.name)
-        ) {
+        if (path.node.tag.name === tag) {
           if (path.node.quasi.quasis.length === 1) {
             path.replaceWith(
               types.objectExpression(
