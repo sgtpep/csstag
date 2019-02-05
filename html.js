9fb7660725af6c85271cb5962e471f845254716c
Danil Semelenov <mail@danil.mobi>
2019-02-05 20:18:01 +0300

    Update a path

--- html.js
+++ html.js
@@ -3,5 +3,5 @@ import { h } from './node_modules/preact/dist/preact.mjs';
 
 export * from './node_modules/preact/dist/preact.mjs';
 export const html = htm.bind(h);
-export { default as css, append as appendStyles } from './index.js';
+export { default as css, append as appendStyles } from './index.min.js';
 export default html;
