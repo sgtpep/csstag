b24c8671a0179c274b3e911ae8affe320ed9a416
Danil Semelenov <mail@danil.mobi>
2019-02-05 03:03:32 +0300

    Modify a function

--- css.js
+++ css.js
@@ -1,6 +1,11 @@
 import requireCSSModule from './require-css-module.js';
 
-export default (strings, ...keys) =>
+export default (strings, ...keys) => {
+  const styles = requireCSSModule(
     strings
       .map((string, index) => (index ? keys[index - 1] : '') + string)
       .join('')
+  );
+  console.log(styles.toString());
+  return styles;
+};
