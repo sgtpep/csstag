073118878a596da00321cd46911cdac430c05695
Danil Semelenov <mail@danil.mobi>
2019-02-05 03:04:19 +0300

    Fix an app

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-import requireCSSModule from './require-css-module.js';
+import App from './App.js';
+import html, { render } from './html.js';
 
-const css = `
-.root {
-  color: red;
-}
-`;
-const styles = requireCSSModule(css);
-console.log(styles);
-console.log(styles.toString());
-
-//console.log(requireCSSModule(, {}));
-//import App from './App.js';
-//import html from './html.js';
-//import { render } from '../node_modules/preact/dist/preact.mjs';
-//
-//render(
-//  html`
-//    <App/ >
-//  `,
-//  document.body
-//);
+render(
+  html`
+    <${App}/ >
+  `,
+  document.body
+);
