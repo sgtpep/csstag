f96fb6686c0f088a41383066b3d9a039f0f5e6c9
Danil Semelenov <mail@danil.mobi>
2019-02-06 03:02:07 +0300

    Remove a demo files

--- html.js
+++
@@ -1,7 +0,0 @@
-import htm from './node_modules/htm/dist/htm.mjs';
-import { h } from './node_modules/preact/dist/preact.mjs';
-
-export * from './node_modules/preact/dist/preact.mjs';
-export const html = htm.bind(h);
-export { default as css, append as appendStyles } from './index.min.js';
-export default html;
